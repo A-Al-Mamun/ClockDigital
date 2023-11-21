0e3b3c294d63639c4d5a060b1382c56cd6ce1d9c
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-11-22 00:03:11 +0600

    Add external js file setup

---
+++ main.js
@@ -0,0 +1,31 @@
+const hour = document.getElementById('hour');
+const minute = document.getElementById('min');
+const second = document.getElementById('sec');
+const ampmE = document.getElementById('ampm');
+
+function updateClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if( h > 12){
+        h = h - 12;
+        ampm = "PM";
+    }
+
+    h = h<10? "0" + h : h;
+    m = m<10? "0" + m : m;
+    s = s<10? "0" + s : s;
+
+    hour.innerText = h;
+    minute.innerText = m;
+    second.innerText = s;
+    ampmE, (innerText = ampm);
+    
+    setTimeout(()=>{
+        updateClock()
+    },1000)
+}
+updateClock()
+// setInterval(updateClock, 1000);
